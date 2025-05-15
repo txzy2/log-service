641d224e914ce8925a4c2dc4192c88372e8b9b61
txzy <kamaeff2@gmail.com>
2025-05-15 21:42:27 +0300

    убрал роуты фронтенда

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,15 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-
-    Route::get('/incidents', function () {
-        return Inertia::render('Incidents');
-    })->name('incidents');
+    abort(404);
 });
 
 Route::get('/openapi', function () {
     return view('redoc');
+});
+
+Route::fallback(function () {
+    abort(404);
 });
