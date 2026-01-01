00dd806c770fa0bc5678539ebcdcbac046d92bc3
KamaevAnton <kamant@otr-soft.rarus.ru>
2026-01-01 13:28:30 +0300

    сделал демо флаг

--- app/Http/Controllers/v1/LogController.php
+++ app/Http/Controllers/v1/LogController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\v1;
 
 use App\Enums\ErrorsEnum;
+use App\Enums\LevelsEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreLogRequest;
 use App\Jobs\WriteIncident;
 use App\Models\Incident;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Throwable;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class LogController extends Controller {
     /**
@@ -18,13 +21,41 @@ class LogController extends Controller {
      * @param StoreLogRequest $request
      * @return JsonResponse
      */
-    public function addLog(StoreLogRequest $request): JsonResponse {
-        Log::channel('debug')->info(static::getControllerClass() . ':addLog RAW REQUEST', [$request->validated()]);
+    public function addLog(Request $request): JsonResponse {
+        $data = $request->all();
+        Log::channel('debug')->info(static::getControllerClass() . ':addLog RAW REQUEST', $data);
+        $validate = Validator::make(
+            $data,
+            [
+                'level' => ['required', Rule::in(LevelsEnum::cases())],
+                'service' => 'required|string',
+                'message' => 'required|string',
+                'domain' => 'required|string',
+                'action' => 'required|string',
+                'function' => 'required|string',
+                'additionalFields' => 'nullable|array',
+                'file' => 'required|string',
+                'class' => 'required|string',
+                'date' => 'required|date',
+                'hash_sum' => 'required|string',
+                'demo' => 'nullable|in:Y,N'
+            ],
+            [
+                'required' => 'Поле :attribute обязательно для заполнения',
+                'additionalFields.array' => 'Неверный тип для additionalFields. ожидается массив',
+                'level.in' => 'Переданный статус не валиден',
+            ],
+        );
 
-        if(isset($request->demo) && !empty($request->demo) && $request->demo === 'Y') {
-            return $this->proccessTestRequest($request);
+        if ($validate->fails()) {
+            Log::channel('debug')->warning(static::getControllerClass() . '::sendReport VALIDATION ERROR', $validate->errors()->all());
+            return $this->sendError($validate->errors(), 400);
+        }
+     
+        if(isset($data['demo']) && !empty($data['demo']) && $data['demo'] === 'Y') {
+            return $this->proccessTestRequest($data);
         } else {
-            return $this->proccessWithQueue($request);
+            return $this->proccessWithQueue($data);
         }
    
     }
@@ -35,8 +66,8 @@ class LogController extends Controller {
      * @param StoreLogRequest $request
      * @return \Illuminate\Http\JsonResponse
      */
-    private function proccessWithQueue(StoreLogRequest $request): JsonResponse {
-        WriteIncident::dispatch($request->validated())->onQueue('writeIncidentLog');
+    private function proccessWithQueue(array $data): JsonResponse {
+        WriteIncident::dispatch($data)->onQueue('writeIncidentLog');
         return $this->sendSuccess(ErrorsEnum::SUCCESS->getMessage());
     }
 
@@ -46,8 +77,8 @@ class LogController extends Controller {
      * @param StoreLogRequest $request
      * @return \Illuminate\Http\JsonResponse
      */
-    private function proccessTestRequest(StoreLogRequest $request): JsonResponse {
-        $incident = Incident::fromArray($request->validated());
+    private function proccessTestRequest(array $data): JsonResponse {
+        $incident = Incident::fromArray($data);
         $result = $incident->process();
 
         if (!$result['success']) {
--- routes/api.php
+++ routes/api.php
@@ -9,9 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/test', [TestConroller::class, 'test']);
 
-// TODO: Раскомментировать после разработки
 Route::prefix('v1')->group(function () {
-    // Работа с логами
     Route::prefix('log')->middleware(ServcieCheck::class)->group(function () {
         Route::post('/', [LogController::class, 'addLog'])->middleware(TokenCheck::class);
     });
